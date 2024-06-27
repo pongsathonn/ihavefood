f547f3beab311429a2824fc9bda66eff0e9bff24
KenMiles <elle.pongsathon@gmail.com>
2024-06-28 00:31:05 +0700

    a

---
+++ src/user/genproto.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -eu
+
+
+<<comment
+ -I = where protoc search for imports
+ --go_out is  Destination directory
+ --go-grpc_out is for complie gRPC stub
+ --grpc-gateway_out is for complie gRPC-gateway 
+
+    **for gRPC-gateway copy these files from googleapis
+        google/api/annotations.proto
+        google/api/field_behavior.proto
+        google/api/http.proto
+        google/api/httpbody.proto
+
+comment
+
+
+# ensure that you've set path for protoc
+PATH=$PATH:$GOPATH/bin
+
+# TODO: user absolute path for this 
+protodir=../../protos # .proto file
+
+protoc -I $protodir --go_out=. --go-grpc_out=. --grpc-gateway_out=. $protodir/food.proto
+
+       
+
