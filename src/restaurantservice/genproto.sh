e0c23f57ecca8ef3e2702b9249943a88c4cbdc48
KenMiles <elle.pongsathon@gmail.com>
2024-11-22 00:20:58 +0700

    delete genproto.sh

--- src/restaurantservice/genproto.sh
+++
@@ -1,7 +0,0 @@
-# ensure that you've set path for protoc
-PATH=$PATH:$GOPATH/bin
-
-protodir=../../protos # .proto file
-
-protoc -I $protodir --go_out=. --go-grpc_out=. --grpc-gateway_out=. $protodir/food.proto
-
