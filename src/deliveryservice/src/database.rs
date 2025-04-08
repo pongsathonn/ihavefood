72e4aca4b61da64b937ead1bd7aa0179a63fec4c
KenMiles <elle.pongsathon@gmail.com>
2025-04-09 00:23:59 +0700

    .

--- src/deliveryservice/src/database.rs
+++ src/deliveryservice/src/database.rs
@@ -1,5 +1,7 @@
 use crate::models::*;
 use anyhow::Result;
+use chrono::offset::Utc;
+use chrono::DateTime;
 use sqlx::sqlite::SqlitePool;
 
 #[derive(Debug)]
@@ -38,6 +40,58 @@ impl Db {
         Ok(())
     }
 
+    pub async fn get_delivery(&self, order_id: String) -> Result<DbDelivery> {
+        let foo = sqlx::query!(
+            r#"
+        SELECT
+            deliveries.order_id AS "order_id:String",
+            deliveries.rider_id AS "rider_id:i32",
+            deliveries.pickup_code AS "pickup_code:String",
+            deliveries.pickup_lat AS "pickup_lat:f64",
+            deliveries.pickup_lng AS "pickup_lng:f64",
+            deliveries.drop_off_lat AS "drop_off_lat:f64",
+            deliveries.drop_off_lng AS "drop_off_lng:f64",
+            deliveries.status AS "status:DbDeliveryStatus",
+            deliveries.create_time AS "create_time:DateTime<Utc>",
+            deliveries.accept_time AS "accept_time:DateTime<Utc>",
+            deliveries.deliver_time AS "deliver_time:DateTime<Utc>",
+            riders.name AS "rider_name:String",
+            riders.phone_number AS "rider_phone_number:String"
+        FROM deliveries
+        LEFT JOIN riders
+        ON deliveries.rider_id = riders.id
+        WHERE deliveries.order_id = ?
+        "#,
+            order_id
+        )
+        .fetch_one(&self.pool)
+        .await?;
+
+        Ok(DbDelivery {
+            order_id: foo.order_id,
+            rider: Some(DbRider {
+                id: foo.rider_id,
+                name: foo.rider_name,
+                phone_number: foo.rider_phone_number,
+            }),
+            pickup_code: foo.pickup_code,
+            pickup_location: DbPoint {
+                latitude: foo.pickup_lat,
+                longitude: foo.pickup_lng,
+            },
+            drop_off_location: DbPoint {
+                latitude: foo.drop_off_lat,
+                longitude: foo.drop_off_lng,
+            },
+            status: foo.status,
+            timestamp: DbTimestamp {
+                create_time: foo.create_time,
+                accept_time: foo.accept_time,
+                deliver_time: foo.deliver_time,
+            },
+        })
+    }
+
     pub async fn get_delivery_status(&self, order_id: String) -> Result<DbDeliveryStatus> {
         let status = sqlx::query_scalar!(
             r#" SELECT status AS "status!:DbDeliveryStatus"  FROM deliveries WHERE order_id=? "#,
@@ -50,7 +104,6 @@ impl Db {
 
     // update the rider whos accepted the order.
     pub async fn update_delivery_rider(&self, update: &DbUpdateDeliveryRider) -> Result<()> {
-        let status = update.status.to_string();
         sqlx::query!(
             r#"
             UPDATE deliveries
@@ -62,7 +115,7 @@ impl Db {
             "#,
             update.order_id,
             update.rider_id,
-            status,
+            update.status,
             update.accept_time,
         )
         .execute(&self.pool)
