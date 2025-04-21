0fd388c6121ceab3fbb0ef8d1d04c1a56e30952c
KenMiles <elle.pongsathon@gmail.com>
2025-04-22 01:14:34 +0700

    x

--- src/deliveryservice/build.rs
+++ src/deliveryservice/build.rs
@@ -1,4 +1,32 @@
+use tonic_build::*;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("../../protos/deliveryservice.proto")?;
+    configure()
+        .type_attribute(
+            "PlaceOrder",
+            "#[derive(serde::Deserialize, serde::Serialize)]",
+        )
+        .type_attribute("Menu", "#[derive(serde::Deserialize, serde::Serialize)]")
+        .type_attribute("Address", "#[derive(serde::Deserialize, serde::Serialize)]")
+        .type_attribute(
+            "ContactInfo",
+            "#[derive(serde::Deserialize, serde::Serialize)]",
+        )
+        .type_attribute(
+            "OrderTimestamps",
+            "#[derive(serde::Deserialize, serde::Serialize)]",
+        )
+        .compile_protos(
+            &[
+                "../../protos/orderservice.proto",
+                "../../protos/deliveryservice.proto",
+            ],
+            &["../../protos"],
+        )?;
+
     Ok(())
 }
+
+// # use tonic_build::*;
+// let mut attributes = Attributes::default();
+// attributes.push_struct("EchoService", "#[derive(PartialEq)]");
