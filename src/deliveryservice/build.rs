c5c330c633cda867aa3945db626ee98b8ac28cfa
KenMiles <elle.pongsathon@gmail.com>
2025-05-12 21:13:52 +0700

    update build

--- src/deliveryservice/build.rs
+++ src/deliveryservice/build.rs
@@ -2,20 +2,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // NOTE:
     // - set at $CARGO_HOME/config.toml
     // - ensure /genproto dir is created.
-<<<<<<< HEAD
-    if let Ok(v) = std::env::var("BUILD_MACHINE") {
-        if v == "local" {
-            tonic_build::configure()
-                .out_dir("genproto")
-                .compile_protos(
-                    &[
-                        "../../protos/orderservice.proto",
-                        "../../protos/deliveryservice.proto",
-                    ],
-                    &["../../protos"],
-                )?;
-        }
-=======
     if std::env::var("BUILD_MACHINE")? == "local" {
         tonic_build::configure()
             .out_dir("genproto")
@@ -26,7 +12,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 ],
                 &["../../protos"],
             )?;
->>>>>>> 662b2561f3aae9c11e203a2b92997f242dc19b49
     }
 
     Ok(())
