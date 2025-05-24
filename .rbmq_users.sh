c20aa7f3cc5e3fd464a9e72f7b72a74c8b4a602b
KenMiles <elle.pongsathon@gmail.com>
2025-05-25 02:55:15 +0700

    create rabbitmq services users script

--- .rbmq_users.sh
+++ .rbmq_users.sh
@@ -1,26 +1,23 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+
+# ensure that rabbitmq server is started before running commands.
 rabbitmq-server &
 RABBITMQ_PID=$!
 rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit@$(hostname).pid --timeout 60
 
 rabbitmqctl add_vhost /
-rabbitmqctl add_user "$RBMQ_ORDER_USER" "$RBMQ_ORDER_PASS"
-rabbitmqctl add_user "$RBMQ_RESTAURANT_USER" "$RBMQ_RESTAURANT_PASS"
-rabbitmqctl add_user "$RBMQ_PROFILE_USER" "$RBMQ_PROFILE_PASS"
-rabbitmqctl add_user "$RBMQ_AUTH_USER" "$RBMQ_AUTH_PASS"
-rabbitmqctl add_user "$RBMQ_DELIVERY_USER" "$RBMQ_DELIVERY_PASS"
-rabbitmqctl add_user "$RBMQ_COUPON_USER" "$RBMQ_COUPON_PASS"
 
-for user in \
-  "$RBMQ_ORDER_USER" \
-  "$RBMQ_RESTAURANT_USER" \
-  "$RBMQ_PROFILE_USER" \
-  "$RBMQ_AUTH_USER" \
-  "$RBMQ_DELIVERY_USER" \
-  "$RBMQ_COUPON_USER"
-do
-  rabbitmqctl set_permissions -p / "$user" ".*" ".*" ".*"
-done
+panda() {
+  if ! rabbitmqctl list_users | grep -q "$1"; then
+    rabbitmqctl add_user "$1" "$2"
+    rabbitmqctl set_permissions -p / "$1" ".*" ".*" ".*"
+  fi
+}
+
+panda "$RBMQ_ORDER_USER" "$RBMQ_ORDER_PASS"
+panda "$RBMQ_RESTAURANT_USER" "$RBMQ_RESTAURANT_PASS"
+panda "$RBMQ_PROFILE_USER" "$RBMQ_PROFILE_PASS"
+panda "$RBMQ_DELIVERY_USER" "$RBMQ_DELIVERY_PASS"
+panda "$RBMQ_COUPON_USER" "$RBMQ_COUPON_PASS"
 
 wait $RABBITMQ_PID
