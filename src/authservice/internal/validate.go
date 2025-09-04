69c6223a35994c33a99f2148155747646cbef064
pongsathon <elle.pongsathon@gmail.com>
2025-09-04 16:18:58 +0700

    add role enum validator

--- src/authservice/internal/validate.go
+++ src/authservice/internal/validate.go
@@ -36,8 +36,9 @@ func SetupValidator() {
 	validate.RegisterStructValidationMapRules(map[string]string{
 		"Username":    "required,min=6,max=16,lowercase",
 		"Email":       "required,email",
-		"Password":    "required,vfpass,min=8,max=16",
-		"PhoneNumber": "required,vfphone",
+		"Password":    "required,vpass,min=8,max=16",
+		"PhoneNumber": "required,vphone",
+		"Role":        "vrole",
 	}, pb.RegisterRequest{})
 
 	validate.RegisterStructValidationMapRules(map[string]string{
@@ -47,10 +48,13 @@ func SetupValidator() {
 
 	// validate.RegisterStructValidationMapRules(rule2, nil)
 
-	// prefix vf = validate format
-	// Ex. vfpass validates password format
-	validate.RegisterValidation("vfpass", validatePassword)
-	validate.RegisterValidation("vfphone", validatePhone)
+	// prefix 'v' for custom validation.
+	// Examples:
+	// - vpass validates password format.
+	// - vrole validates roles enum.
+	validate.RegisterValidation("vpass", validatePassword)
+	validate.RegisterValidation("vphone", validatePhone)
+	validate.RegisterValidation("vrole", validateRole)
 }
 
 func ValidateStruct(in any) error {
@@ -76,19 +80,49 @@ func buildMyValidatorErr(f validator.FieldError) myValidatorErr {
 		return myValidatorErr{Field: f.Field(), Msg: "is required"}
 	case "email":
 		return myValidatorErr{Field: f.Field(), Msg: "must be a valid email address"}
-	case "vfpass":
-		return myValidatorErr{Field: f.Field(), Msg: "must contain lowercase,uppercase and special character"}
-	case "vfphone":
-		return myValidatorErr{Field: f.Field(), Msg: "must be a valid phone number format"}
 	case "min":
 		return myValidatorErr{Field: f.Field(), Msg: fmt.Sprintf("must be at least %s", f.Param())}
 	case "max":
 		return myValidatorErr{Field: f.Field(), Msg: fmt.Sprintf("must be at most %s", f.Param())}
 	case "lowercase":
 		return myValidatorErr{Field: f.Field(), Msg: "must be lowercase only"}
+	case "vpass":
+		return myValidatorErr{Field: f.Field(), Msg: "must contain lowercase,uppercase and special character"}
+	case "vphone":
+		return myValidatorErr{Field: f.Field(), Msg: "must be a valid phone number format"}
+	case "vrole":
+		var roles []string
+		for role := range pb.Roles_value {
+			if role != pb.Roles_UNKNOWN.String() {
+				roles = append(roles, role)
+			}
+		}
+		return myValidatorErr{
+			Field: f.Field(),
+			Msg:   fmt.Sprintf("must be one of %s", strings.Join(roles, ", ")),
+		}
 	default:
 		return myValidatorErr{Field: f.Field(), Msg: fmt.Sprintf("invalid value tag %s", f.Tag())}
 	}
+}
+
+func validateRole(fl validator.FieldLevel) bool {
+
+	r := fl.Field().Interface().(pb.Roles)
+	if r == pb.Roles_UNKNOWN {
+		return false
+	}
+
+	_, exists := pb.Roles_value[r.String()]
+	return exists
+}
+
+// phone number format (e.g., 06XXXXXXXX, 08XXXXXXXX, 09XXXXXXXX).
+// Any format outside of this is considered invalid, and the function
+// returns an error.
+func validatePhone(fl validator.FieldLevel) bool {
+	phoneNumber := fl.Field().String()
+	return regexp.MustCompile(`^(06|08|09)\d{8}$`).MatchString(phoneNumber)
 }
 
 func validatePassword(fl validator.FieldLevel) bool {
@@ -96,14 +130,6 @@ func validatePassword(fl validator.FieldLevel) bool {
 	return regexp.MustCompile(`[a-z]`).MatchString(password) &&
 		regexp.MustCompile(`[A-Z]`).MatchString(password) &&
 		regexp.MustCompile(`[!.@#$%^&*()_\-+=<>?]`).MatchString(password)
-}
-
-func validatePhone(fl validator.FieldLevel) bool {
-	phoneNumber := fl.Field().String()
-	// phone number format (e.g., 06XXXXXXXX, 08XXXXXXXX, 09XXXXXXXX).
-	// Any format outside of this is considered invalid, and the function
-	// returns an error.
-	return regexp.MustCompile(`^(06|08|09)\d{8}$`).MatchString(phoneNumber)
 }
 
 // verifyUserToken verifies the validity of a JWT token using the provided signing key.
