9526fc8b1462b94319b3d956fa2e1cbd02496669
pongsathon <elle.pongsathon@gmail.com>
2025-09-04 19:38:55 +0700

    ,

--- protos/tempgenproto.sh
+++ protos/tempgenproto.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-SERVICES=('customer' 'auth' 'merchant' 'delivery' 'order' 'coupon')
+# # TODO add deps for each services
 
-# TODO add deps for each services
+SERVICES=('customerservice' 'authservice' 'merchantservice' 'orderservice' 'couponservice')
 
-path=../src/
+service_path=../src/
 gateway_path=../api-gateway/
 
-for s in "${SERVICES[@]}"; do
-    s="${s}service"
+for i in "${!SERVICES[@]}"; do
+    out="$service_path${SERVICES[$i]}"
+    if [[ $i -eq $((${#SERVICES[@]} - 1)) ]]; then
+        out="$gateway_path"
+    fi
+
+    protos=("common.proto" "${SERVICES[@]/%/.proto}")
     protoc -I "." \
-        --go_out="$path$s" \
-        --go-grpc_out="$path$s" \
-        --grpc-gateway_out="$path$s" \
+        --go_out="$out" \
+        --go-grpc_out="$out" \
+        --grpc-gateway_out="$out" \
         --openapiv2_out=. \
         --openapiv2_opt=openapi_naming_strategy=simple,allow_merge=true,logtostderr=true,disable_default_errors=true \
-        "common.proto" \
-        "customerservice.proto" \
-        "authservice.proto" \
-        "merchantservice.proto" \
-        "deliveryservice.proto" \
-        "orderservice.proto" \
-        "couponservice.proto"
+        "${protos[@]}"
 done
-
-
 
