c3f5411cb39d2de28c8346fcca41ff911813e98b
pongsathon <elle.pongsathon@gmail.com>
2026-03-10 05:48:17 +0700

    impl logout button

--- web/components/dropdown.tsx
+++ web/components/dropdown.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { logout } from '@/app/(auth)/actions'
 import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
@@ -6,15 +9,16 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
-import {
-  AlignJustifyIcon,
-  Braces,
-  CreditCard,
-  House,
-  LogOutIcon,
-} from 'lucide-react'
+import { AlignJustifyIcon, Braces, LogOutIcon } from 'lucide-react'
+import { useTransition } from 'react'
 
 export default function DropdownMenuIcons() {
+  // useTransition
+  // Provides a 'true/false' loading state to disable buttons or show spinners.
+  // Safely handles the UI if the user navigates away before the action finishes.
+  // Prevents 'race conditions' and bugs common with raw async event listeners.
+  const [isPending, startTransition] = useTransition()
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -26,22 +30,30 @@ export default function DropdownMenuIcons() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem className="cursor-pointer">
+        {/*<DropdownMenuItem className="cursor-pointer">
           <House />
           My Address
         </DropdownMenuItem>
         <DropdownMenuItem className="cursor-pointer">
           <CreditCard />
           Payment
-        </DropdownMenuItem>
+        </DropdownMenuItem>*/}
         <DropdownMenuItem className="cursor-pointer">
           <Braces />
           OpenAPI
         </DropdownMenuItem>
         <DropdownMenuSeparator />
-        <DropdownMenuItem variant="destructive" className="cursor-pointer">
+        <DropdownMenuItem
+          variant="destructive"
+          className="cursor-pointer"
+          onClick={() => {
+            startTransition(async () => {
+              await logout()
+            })
+          }}
+        >
           <LogOutIcon />
-          Log out
+          {isPending ? 'Logging out...' : 'Log Out'}
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
