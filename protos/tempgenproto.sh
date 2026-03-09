fa3132abfe7aef563ccecfda172be11e30db185a
pongsathon <elle.pongsathon@gmail.com>
2026-03-09 14:08:55 +0700

    change openapi output dir

--- protos/tempgenproto.sh
+++ protos/tempgenproto.sh
@@ -18,12 +18,10 @@ for i in "${!SERVICES[@]}"; do
         --go_out="$out" \
         --go-grpc_out="$out" \
         --grpc-gateway_out="$out" \
-        --openapiv2_out=../web/public/openapi \
+        --openapiv2_out=../web/openapi \
         --openapiv2_opt=openapi_naming_strategy=simple,allow_merge=true,logtostderr=true,disable_default_errors=true \
         "${protos[@]}"
 done
 
 # find ~/workspace/ihavefood -type d -name "genproto" -exec git add {}/* \;
 # git commit -m "update protobuf" || echo "No changes to commit"
-
-
